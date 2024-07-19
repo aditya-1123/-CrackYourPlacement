35c115dcd1e641b90b5b51c58553b088dd14844a
Aditya Gaikwad <gaikwadaditya1123@gmail.com>
2024-07-19 13:56:07 +0530

    Create Sort Colors.cpp

---
+++ Day 1/Sort Colors.cpp
@@ -0,0 +1,20 @@
+// Sort Colors
+
+class Solution {
+public:
+    void sortColors(vector<int>& arr) { // O(n) 
+        int n = arr.size(); 
+        int l=0; int h=n-1; int mid=0;
+        
+        while(mid <= h)
+        {
+            if(arr[mid] == 0)
+                swap(arr[l++], arr[mid++]);
+                
+            else if(arr[mid] == 1)
+                mid++;
+            else
+                swap(arr[mid], arr[h--]);
+        }
+    }
+};
