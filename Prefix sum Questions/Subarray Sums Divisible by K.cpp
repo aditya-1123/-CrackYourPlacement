4c79bc1c76810a4668baad4fcb67d47d8f00d20e
Aditya Gaikwad <gaikwadaditya1123@gmail.com>
2024-07-22 10:35:37 +0530

    Create Subarray Sums Divisible by K.cpp

---
+++ Prefix sum Questions/Subarray Sums Divisible by K.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+
+// Idea prefix sum
+
+int subarraysDivByK(vector<int>& nums, int k) {
+        unordered_map<int, int>m;
+
+        int ans = 0;
+        int sum = 0;
+        int rem = 0;
+
+        m[rem]++;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            rem = sum % k;
+
+            if(rem < 0) {
+                rem += k;
+            }
+
+            if(m.find(rem) != m.end()) {
+                ans += m[rem];
+            }
+
+            m[rem]++;
+        }
+
+        return ans;
+    }
