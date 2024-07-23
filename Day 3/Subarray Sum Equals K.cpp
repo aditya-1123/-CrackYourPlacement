4547a591843c5847edbcb19ca785b61a12bb0c5f
Aditya Gaikwad <gaikwadaditya1123@gmail.com>
2024-07-23 20:35:22 +0530

    Update Subarray Sum Equals K.cpp

--- Day 3/Subarray Sum Equals K.cpp
+++ Day 3/Subarray Sum Equals K.cpp
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/subarray-sum-equals-k/description/
 // Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 // A subarray is a contiguous non-empty sequence of elements within an array.
 
