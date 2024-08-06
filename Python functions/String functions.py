ee34fa01697ebe6bfc53a78ba77d681393b096f7
Aditya Gaikwad <gaikwadaditya1123@gmail.com>
2024-08-06 21:00:50 +0530

    Create String functions.py

---
+++ Python functions/String functions.py
@@ -0,0 +1,23 @@
+s = "Aditya"
+
+# print(s.upper())
+print(s.lower())
+
+s = "  Aditya  "
+print(s.strip())
+
+s = "Aditya Gaikwad"
+print(s.split())
+
+print(s.count('a')) # no of times 'a' appears
+
+print(s.find('itya'))
+
+print(len(s))
+
+-----
+ch = 'A'
+print(ch.isupper())
+
+ch = 'a'
+print(ch.islower())
